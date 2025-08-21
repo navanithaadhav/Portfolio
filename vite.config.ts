a8f847ed3cb5a8209ebe94958ff101b6d5e799a9
navanithaadhav <navanithaweb@gmail.com>
2025-08-21 13:14:59 +0530

    Normalize line endings

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: process.env.VITE_BASE_PATH || '/', // Set base path for deployment
+
 });
